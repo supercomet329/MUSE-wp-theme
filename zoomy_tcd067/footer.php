2330320f9cec30b7d8127cefff77cfe1797244ef
supercomet329 <supercomet329@gmail.com>
2022-05-06 17:18:45 +0900

    # フッターのログイン判定処理の追加

--- zoomy_tcd067/footer.php
+++ zoomy_tcd067/footer.php
@@ -2,6 +2,7 @@
 global $dp_options;
 if (!$dp_options) $dp_options = get_design_plus_option();
 ?>
+<?php if ( is_user_logged_in() ) { ?>
 <footer class="p-b-10">
 	<div class="navbar  navbar-light bg-light shadow fixed-bottom">
 		<a href="normal_mode.html"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon/home.png" alt="home" width="100" height="100"></a>
@@ -16,6 +17,8 @@ if (!$dp_options) $dp_options = get_design_plus_option();
 		<!-- <strong>Message</strong> -->
 	</div>
 </footer>
+<?php } ?>
+
 </body>
 
 </html>
