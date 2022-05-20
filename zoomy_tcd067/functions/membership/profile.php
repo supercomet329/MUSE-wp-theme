336c1a54410433af8042913fe1024466025dd396
supercomet329 <supercomet329@gmail.com>
2022-05-20 17:25:49 +0900

    # プロフィールページの対応

---
+++ zoomy_tcd067/functions/membership/profile.php
@@ -0,0 +1,10 @@
+<?php
+// Add 2022/05/10 by H.Okabe
+/**
+ * 利用規約
+ */
+function tcd_membership_action_profile()
+{
+    var_dump(__LINE__);exit;
+}
+add_action('tcd_membership_action-profile', 'tcd_membership_action_profile');
