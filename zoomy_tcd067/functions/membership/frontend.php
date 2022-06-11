e96509bf2779e058324bf4dbd4ae0334ce046370
supercomet329 <supercomet329@gmail.com>
2022-06-11 21:03:45 +0900

    # 依頼一覧の登録

--- zoomy_tcd067/functions/membership/frontend.php
+++ zoomy_tcd067/functions/membership/frontend.php
@@ -416,6 +416,102 @@ function listOrder($up_budget, $down_budget, $whereDeadLine, $target)
 }
 
 /**
+ * 依頼一覧の取得
+ */
+function lisetOrder($sort, $search_txt, $sel_up_budget, $sel_down_budget, $sel_limit)
+{
+	global $wpdb;
+
+	$sql = '';
+	$sql .= 'SELECT ';
+	$sql .= ' DISTINCT wp_posts.ID AS post_id ';
+	$sql .= ',wp_posts.post_author AS post_author ';
+	$sql .= ',wp_posts.post_date AS post_date ';
+	$sql .= ',wp_posts.post_title AS post_title ';
+	$sql .= ',wp_posts.post_content AS post_content ';
+	$sql .= ',wp_users.display_name AS display_name ';
+
+	$sql .= ',(';
+	$sql .= ' SELECT meta_value FROM wp_postmeta WHERE wp_postmeta.post_id = wp_posts.ID AND meta_key = \'appDeadlineDate\'';
+	$sql .= ') AS appDeadlineDate ';
+
+	$sql .= ',(';
+	$sql .= ' SELECT meta_value FROM wp_postmeta WHERE wp_postmeta.post_id = wp_posts.ID AND meta_key = \'budget\'';
+	$sql .= ') AS budget ';
+
+	$sql .= 'FROM wp_posts ';
+	$sql .= ' INNER JOIN wp_users ';
+	$sql .= ' ON wp_users.ID = wp_posts.post_author ';
+	$sql .= 'WHERE wp_posts.post_type = \'request\' ';
+	$sql .= ' AND wp_posts.post_status = \'publish\'';
+	$sql .= ' AND NOT EXISTS(';
+	$sql .= ' SELECT * 
+				FROM wp_tcd_membership_actions 
+				WHERE 
+					wp_tcd_membership_actions.post_id = wp_posts.ID 
+				AND 
+					wp_tcd_membership_actions.type = \'received\' 
+				';
+	$sql .= ' ) ';
+	$sql .= ' AND EXISTS(';
+	$sql .= ' 	SELECT * 
+				FROM wp_postmeta 
+				WHERE 
+					wp_postmeta.post_id = wp_posts.ID 
+				AND 
+					wp_postmeta.meta_key = \'appDeadlineDate\' 
+				AND 
+					wp_postmeta.meta_value > NOW()
+	';
+	$sql .= ')';
+
+	if(! empty($sel_up_budget)) {
+		$sql .= ' AND (';
+		$sql .= ' SELECT meta_value FROM wp_postmeta WHERE wp_postmeta.post_id = wp_posts.ID AND meta_key = \'budget\'';
+		$sql .= ') <= ' . $sel_up_budget;
+	}
+
+	if(! empty($sel_down_budget)) {
+		$sql .= ' AND (';
+		$sql .= ' SELECT meta_value FROM wp_postmeta WHERE wp_postmeta.post_id = wp_posts.ID AND meta_key = \'budget\'';
+		$sql .= ') >= ' . $sel_down_budget;
+	}
+
+	if(! empty($sel_limit)) {
+		$dateClass = new DateTime();
+		$dateClass->modify('+' . $sel_limit);
+
+		$sql .= ' AND (';
+		$sql .= ' SELECT meta_value FROM wp_postmeta WHERE wp_postmeta.post_id = wp_posts.ID AND meta_key = \'appDeadlineDate\'';
+		$sql .= ') <= \'' . $dateClass->format('Y-m-d 23:59:59') .'\'';
+	}
+
+	if(!empty($search_txt)) {
+		$sql .= ' AND (
+			wp_posts.post_title LIKE \'%' . $search_txt . '%\'
+			OR
+			wp_posts.post_content LIKE \'%' . $search_txt . '%\'
+			OR
+			wp_posts.post_name LIKE \'%' . $search_txt . '%\'
+		)';
+	}
+
+	if($sort === 'id_old') {
+		$sql .= ' ORDER BY wp_posts.ID ASC';
+	} else if($sort === 'budget_down') {
+		$sql .= ' ORDER BY budget ASC';
+	} else if($sort === 'budget_up') {
+		$sql .= ' ORDER BY budget DESC';
+	} else {
+		$sql .= ' ORDER BY wp_posts.ID DESC';
+	}
+
+	// echo $sql;exit;
+	$result = $wpdb->get_results($wpdb->prepare($sql));
+	return $result;
+}
+
+/**
  * 受注一覧の取得
  */
 function listReceivedByUserId($user_id, $up_budget, $down_budget, $whereDeadLine, $target)
---
+++ zoomy_tcd067/membership-template/muse_order_search.php
@@ -0,0 +1,148 @@
+<?php
+global $dp_options, $tcd_membership_vars;
+get_header();
+?>
+<div class="text-center font-weight-bold title mt-4">
+    作品依頼検索
+</div>
+<div class="container mt-3 request-list">
+    <div class="mb-2">
+        <form class="search-post mb-2" method="POST" action="<?php echo esc_url(get_tcd_membership_memberpage_url('order_search')); ?>">
+            <input class="search-box px-2 pb-0" id="search_box" value="<?php echo $tcd_membership_vars['search_txt']; ?>" name="search_txt" type="text" placeholder="検索" />
+            <button class="search-button btn rounded-pill btn-sm font-weight-bold my-auto ml-2" type="submit">検索</button>
+            <img class="modal-open float-right" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon/search_option.png" alt="search_option" class="float-right search-option">
+        </form>
+        <div class="tab d-flex " id="sort_tab">
+            <a href="<?php echo esc_url(get_tcd_membership_memberpage_url('order_search')); ?>&sort=id_new" class="rounded-pill font-weight-bold <?php echo ($tcd_membership_vars['sort'] === 'id_new') ? 'selected-tab' : 'not-selected-tab'; ?> small text-center pt-2" id="desc">新しい順</a>
+            <a href="<?php echo esc_url(get_tcd_membership_memberpage_url('order_search')); ?>&sort=id_old" class="rounded-pill font-weight-bold <?php echo ($tcd_membership_vars['sort'] === 'id_old') ? 'selected-tab' : 'not-selected-tab'; ?> small text-center pt-2" id="asc">古い順</a>
+            <a href="<?php echo esc_url(get_tcd_membership_memberpage_url('order_search')); ?>&sort=budget_down" class="rounded-pill font-weight-bold <?php echo ($tcd_membership_vars['sort'] === 'budget_down') ? 'selected-tab' : 'not-selected-tab'; ?> small text-center pt-2" id="low">予算低い順</a>
+            <a href="<?php echo esc_url(get_tcd_membership_memberpage_url('order_search')); ?>&sort=budget_up" class="rounded-pill font-weight-bold <?php echo ($tcd_membership_vars['sort'] === 'budget_up') ? 'selected-tab' : 'not-selected-tab'; ?> small text-center pt-2" id="high">予算高い順</a>
+        </div>
+    </div>
+    <?php
+    $i = 0;
+    foreach ($tcd_membership_vars['list_order'] as $one_order) {
+
+        $profileImageData = get_user_meta($one_order->post_author, 'profile_image', true);
+        $profile_image = get_template_directory_uri() . '/assets/img/icon/non_profile_image.png';
+        if (!empty($profileImageData)) {
+            $profile_image = $profileImageData;
+        }
+        $requestData['profile_image'] = $profile_image;
+
+        $dateClass = new DateTime($one_order->appDeadlineDate);
+        $appDeadlineDate = $dateClass->format('Y/m/d');
+        if ($i <= 0) {
+            echo '<div class="row">';
+        } else {
+            echo '<div class="row request-boundary">';
+        }
+    ?>
+        <div class="col-12 request-area mt-3">
+            <a href="<?php echo esc_url(get_tcd_membership_memberpage_url('comfirm_request')); ?>&request_id=<?php echo $one_order->post_id; ?>">
+                <div class="request-title font-weight-bold mt-3 mb-2">
+                    <?php echo esc_attr($one_order->post_title); ?>
+                </div>
+            </a>
+            <div class="row">
+                <div class="col-4">
+                    <div class="row">
+                        <div class="col-12 text-center">
+                            <img src="<?php echo esc_url($profile_image); ?>" alt="profile" class="rounded-circle request-user-icon">
+                        </div>
+                        <div class="col-12 text-center requester mt-1">
+                            <p class="font-weight-bold"><?php echo esc_attr($one_order->display_name); ?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-8 pl-1">
+                    <div class="row">
+                        <div class="col-12 h-70px request-detail">
+                            <?php echo nl2br($one_order->post_content); ?>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-3 request-budget px-0 mt-1 ml-3 mr-1">
+                            <p class="font-weight-bold mb-0"><?php echo esc_attr($one_order->budget); ?>円</p>
+                        </div>
+                        <div class="col-3 subtext request-deadline px-0 mx-1 mt-1">
+                            <p class="font-weight-bold mb-0"><?php echo esc_attr($appDeadlineDate); ?></p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-2 px-0 mt-1 ml-3 mr-1 request-keep">
+                            <div class="border rounded-pill text-center mb-1 px-1 keep_off">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon/keep_off.png" alt="keep-off" class="keep-off">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+<?php
+        $i++;
+    }
+    /** endforeach */
+?>
+
+<!-- 検索モーダル -->
+<form class="search-post mb-2" method="POST" action="<?php echo esc_url(get_tcd_membership_memberpage_url('order_search')); ?>">
+    <div class="modal-container">
+        <div class="modal-body">
+            <div class="modal-content text-left p-3">
+                <p class="item-text mx-auto">検索オプション</p>
+                <form class="search-post" method="POST">
+                    <div class="mb-2 item-text">
+                        予算上限
+                    </div>
+                    <div class="select-wrap">
+                        <select name="sel_up_budget" class="select-box mb-3">
+                            <option value="">指定なし</option>
+                            <option value="10000" <?php echo ((int)$tcd_membership_vars['sel_up_budget'] === 10000) ? 'selected': ''; ?>>10000円</option>
+                            <option value="50000" <?php echo ((int)$tcd_membership_vars['sel_up_budget'] === 50000) ? 'selected': ''; ?>>50000円</option>
+                            <option value="100000" <?php echo ((int)$tcd_membership_vars['sel_up_budget'] === 100000) ? 'selected': ''; ?>>100000円</option>
+                        </select>
+                    </div>
+                    <div class="mb-2 item-text">
+                        予算下限
+                    </div>
+                    <div class="select-wrap">
+                        <select name="sel_down_budget" class="select-box mb-3">
+                            <option value="">指定なし</option>
+                            <option value="10000" <?php echo ((int)$tcd_membership_vars['sel_down_budget'] === 10000) ? 'selected': ''; ?>>10000円</option>
+                            <option value="50000" <?php echo ((int)$tcd_membership_vars['sel_down_budget'] === 50000) ? 'selected': ''; ?>>50000円</option>
+                            <option value="100000" <?php echo ((int)$tcd_membership_vars['sel_down_budget'] === 100000) ? 'selected': ''; ?>>100000円</option>
+                        </select>
+                    </div>
+                    <div class="mb-2 item-text">
+                        期日
+                    </div>
+                    <div class="select-wrap">
+                        <select name="sel_limit" class="select-box mb-3">
+                            <option value="">指定なし</option>
+                            <option value="1week" <?php echo ($tcd_membership_vars['sel_limit'] === '1week') ? 'selected': ''; ?>>1週間以内</option>
+                            <option value="1month" <?php echo ($tcd_membership_vars['sel_limit'] === '1month') ? 'selected': ''; ?>>1か月以内</option>
+                            <option value="1year" <?php echo ($tcd_membership_vars['sel_limit'] === '1year') ? 'selected': ''; ?>>1年以内</option>
+                        </select>
+                    </div>
+                    <?php /** TODO: 依頼にR-18指定はないので検索対象から外しました。 */ ?>
+                    <!-- div class="mb-2 item-text">
+                        対象
+                    </div>
+                    <div class="select-wrap">
+                        <select class="select-box mb-3">
+                            <option value="">全年齢</option>
+                            <option value="">R-18</option>
+                        </select>
+                    </div -->
+                    <div class="text-center mt-3">
+                        <button type="submit" class="btn btn-primary search-btn btn-sm btn-lg text-white">検索</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</form>
+<?php
+get_footer();
