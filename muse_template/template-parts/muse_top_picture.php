99069592fa2ce5a45f3b8adc73c531f085a76af5
supercomet329 <supercomet329@gmail.com>
2022-07-14 14:26:18 +0900

    # ランキングの取得ができない場合は => ランキングを表示しない

--- muse_template/template-parts/muse_top_picture.php
+++ muse_template/template-parts/muse_top_picture.php
@@ -2,30 +2,31 @@
 global $dp_options, $post;
 if (!$dp_options) $dp_options = get_design_plus_option();
 ?>
-<?php /** TODO: 人気アーティストの表示 */ ?>
-<ul class="horizontal-list">
-    <li class="item ml-2 ranking-icon-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon/ranking_01.png" class="ranking-icon" alt="ranking_icon"><br /></li>
+<?php if (count(partsRanking()) > 0) { ?>
+    <ul class="horizontal-list">
+        <li class="item ml-2 ranking-icon-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon/ranking_01.png" class="ranking-icon" alt="ranking_icon"><br /></li>
 
-    <?php
-    foreach (partsRanking() as $rankingOne) {
+        <?php
+        foreach (partsRanking() as $rankingOne) {
 
-        $profile_image = get_template_directory_uri() . '/assets/img/icon/non_profile_image.png';
-        if (!is_null($rankingOne->profile_image)) {
-            $profile_image = $rankingOne->profile_image;
-        }
-    ?>
-        <a href="<?php echo esc_url(get_tcd_membership_memberpage_url('profile')) ?>&user_id=<?php echo esc_attr($rankingOne->user_id); ?>">
-            <li class="item">
-                <img src="<?php echo esc_url($profile_image); ?>" alt="profile" class="rounded-circle">
-            </li>
+            $profile_image = get_template_directory_uri() . '/assets/img/icon/non_profile_image.png';
+            if (!is_null($rankingOne->profile_image)) {
+                $profile_image = $rankingOne->profile_image;
+            }
+        ?>
+            <a href="<?php echo esc_url(get_tcd_membership_memberpage_url('profile')) ?>&user_id=<?php echo esc_attr($rankingOne->user_id); ?>">
+                <li class="item">
+                    <img src="<?php echo esc_url($profile_image); ?>" alt="profile" class="rounded-circle">
+                </li>
+            </a>
+        <?php }
+        /** endforeach */ ?>
+        <a href="ranking.html">
+            <li class="item"><img class="transform-x-reverse border border-dark rounded-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon/arrow.png" alt="profile" class="rounded-circle"></li>
         </a>
-    <?php }
-    /** endforeach */ ?>
-    <a href="ranking.html">
-        <li class="item"><img class="transform-x-reverse border border-dark rounded-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon/arrow.png" alt="profile" class="rounded-circle"></li>
-    </a>
-</ul>
-<?php /** TODO: 人気アーティストの表示 */ ?>
+    </ul>
+<?php }
+/** endif */ ?>
 
 <main role="main" class="mb-2">
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
--- muse_template/template-parts/muse_top_timeline.php
+++ muse_template/template-parts/muse_top_timeline.php
@@ -11,28 +11,31 @@ $imageList = getPostImageByPostTypeAndPostStatus();
     </div>
 </div>
 
-<ul class="horizontal-list">
-    <li class="item ml-2 ranking-icon-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon/ranking_01.png" class="ranking-icon" alt="ranking_icon"><br /></li>
+<?php if (count(partsRanking()) > 0) { ?>
+    <ul class="horizontal-list">
+        <li class="item ml-2 ranking-icon-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon/ranking_01.png" class="ranking-icon" alt="ranking_icon"><br /></li>
 
-    <?php
-    foreach (partsRanking() as $rankingOne) {
+        <?php
+        foreach (partsRanking() as $rankingOne) {
 
-        $profile_image = get_template_directory_uri() . '/assets/img/icon/non_profile_image.png';
-        if (!is_null($rankingOne->profile_image)) {
-            $profile_image = $rankingOne->profile_image;
-        }
-    ?>
-        <a href="<?php echo esc_url(get_tcd_membership_memberpage_url('profile')) ?>&user_id=<?php echo esc_attr($rankingOne->user_id); ?>">
-            <li class="item">
-                <img src="<?php echo esc_url($profile_image); ?>" alt="profile" class="rounded-circle">
-            </li>
+            $profile_image = get_template_directory_uri() . '/assets/img/icon/non_profile_image.png';
+            if (!is_null($rankingOne->profile_image)) {
+                $profile_image = $rankingOne->profile_image;
+            }
+        ?>
+            <a href="<?php echo esc_url(get_tcd_membership_memberpage_url('profile')) ?>&user_id=<?php echo esc_attr($rankingOne->user_id); ?>">
+                <li class="item">
+                    <img src="<?php echo esc_url($profile_image); ?>" alt="profile" class="rounded-circle">
+                </li>
+            </a>
+        <?php }
+        /** endforeach */ ?>
+        <a href="ranking.html">
+            <li class="item"><img class="transform-x-reverse border border-dark rounded-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon/arrow.png" alt="profile" class="rounded-circle"></li>
         </a>
-    <?php }
-    /** endforeach */ ?>
-    <a href="ranking.html">
-        <li class="item"><img class="transform-x-reverse border border-dark rounded-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon/arrow.png" alt="profile" class="rounded-circle"></li>
-    </a>
-</ul>
+    </ul>
+<?php }
+/** endif */ ?>
 
 <?php
 foreach ($imageList as $imageOne) {
