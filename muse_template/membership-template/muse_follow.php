7b36adac101de89e29d83b28fabda755e526200e
supercomet329 <supercomet329@gmail.com>
2022-09-21 15:05:07 +0900

    # フォロー・フォロワーの対応

--- muse_template/membership-template/muse_follow.php
+++ muse_template/membership-template/muse_follow.php
@@ -3,66 +3,74 @@ global $dp_options, $tcd_membership_vars;
 
 get_header();
 ?>
-<div class="text-center font-weight-bold title">
-    フォロー一覧
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <ul class="list-area">
-                <?php
-                foreach ($tcd_membership_vars['list_follow'] as $one_follow) {
-                    $user_info = get_userdata($one_follow->target_user_id);
+<div class="pc-center">
 
-                    $description = '';
-                    $descriptionData = get_user_meta($one_follow->target_user_id, 'description', true);
-                    // プロフィールの取得
-                    if (!empty($descriptionData)) {
-                        $description = $descriptionData;
-                    }
 
-                    $profileImageData = get_user_meta($one_follow->target_user_id, 'profile_image', true);
-                    $profile_image = get_template_directory_uri() . '/assets/img/icon/non_profile_image.png';
-                    if (!empty($profileImageData)) {
-                        $profile_image = $profileImageData;
-                    }
-                ?>
-                    <div class="row my-2">
-                        <div class="col-2 d-block d-lg-none pr-0">
-                            <img src="<?php echo $profile_image; ?>" class="rounded-circle profile-icon">
-                        </div>
-                        <div class="col-2 d-none d-lg-block pr-0">
-                            <img src="<?php echo $profile_image; ?>" class="rounded-circle profile-icon">
-                        </div>
-                        <div class="col-6 d-block d-lg-none">
-                            <p class="name font-weight-bold mb-1"><?php echo $user_info->display_name; ?></p>
-                            <p class="profile-detail mb-1"><?php echo $description; ?></p>
-                        </div>
-                        <div class="col-8 d-none d-lg-block">
-                            <p class="name font-weight-bold mb-1"><?php echo $user_info->display_name; ?></p>
-                            <p class="profile-detail mb-1"><?php echo $description; ?></p>
-                        </div>
-                        <div class="col-4 d-block d-lg-none btn-area text-right pl-0 my-auto">
+    <div class="container pt-2">
+        <div class="row mb-2">
+            <div class="col-12">
+                <a href="javascript:history.back();">← 戻る</a>
+            </div>
+        </div>
+        <div class="text-center font-weight-bold title mt-0">
+            フォロー一覧
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <ul class="list-area">
+                    <?php
+                    foreach ($tcd_membership_vars['list_follow'] as $one_follow) {
+                        $user_info = get_userdata($one_follow->target_user_id);
+
+                        $description = '';
+                        $descriptionData = get_user_meta($one_follow->target_user_id, 'description', true);
+                        // プロフィールの取得
+                        if (!empty($descriptionData)) {
+                            $description = $descriptionData;
+                        }
+
+                        $profileImageData = get_user_meta($one_follow->target_user_id, 'profile_image', true);
+                        $profile_image = get_template_directory_uri() . '/assets/img/icon/non_profile_image.png';
+                        if (!empty($profileImageData)) {
+                            $profile_image = $profileImageData;
+                        }
+                    ?>
+                        <div class="row my-2">
+                            <div class="col-2 d-block d-lg-none pr-0">
+                                <img src="<?php echo $profile_image; ?>" class="rounded-circle profile-icon">
+                            </div>
+                            <div class="col-2 d-none d-lg-block pr-0">
+                                <img src="<?php echo $profile_image; ?>" class="rounded-circle profile-icon">
+                            </div>
+                            <div class="col-6 d-block d-lg-none">
+                                <p class="name font-weight-bold mb-1"><?php echo $user_info->display_name; ?></p>
+                                <p class="profile-detail mb-1"><?php echo $description; ?></p>
+                            </div>
+                            <div class="col-8 d-none d-lg-block">
+                                <p class="name font-weight-bold mb-1"><?php echo $user_info->display_name; ?></p>
+                                <p class="profile-detail mb-1"><?php echo $description; ?></p>
+                            </div>
+                            <div class="col-4 d-block d-lg-none btn-area text-right pl-0 my-auto">
                             <?php if (is_following($one_follow->target_user_id)) { ?>
-                                <a href="#" id="follow_button" data-user-id="<?php echo esc_attr($one_follow->target_user_id); ?>" class="js-toggle-follow btn btn-primary rounded-pill btn-sm text-white btn-lg main-color">フォロー中</a>
+                                <a href="#" id="follow_button" data-user-id="<?php echo esc_attr($one_follow->target_user_id); ?>" class="js-toggle-follow btn btn-primary rounded-pill btn-sm text-white btn-lg main-color follow-btn follow-on">フォロー中</a>
                             <?php } else { ?>
-                                <a href="#" id="follow_button" data-user-id="<?php echo esc_attr($one_follow->target_user_id); ?>" class="js-toggle-follow btn rounded-pill btn-outline-primary outline-btn btn-sm">フォローする</a>
+                                <a href="#" id="follow_button" data-user-id="<?php echo esc_attr($one_follow->target_user_id); ?>" class="js-toggle-follow btn py-0 rounded-pill btn-outline-primary btn-sm follow-btn follow-off">フォローする</a>
                             <?php } ?>
-                        </div>
-                        <div class="col-2 d-none d-lg-block btn-area text-right pl-0 my-auto">
+                            </div>
+                            <div class="col-2 d-none d-lg-block btn-area text-right pl-0 my-auto">
                             <?php if (is_following($one_follow->target_user_id)) { ?>
-                                <a href="#" id="follow_button" data-user-id="<?php echo esc_attr($one_follow->target_user_id); ?>" class="js-toggle-follow btn btn-primary rounded-pill btn-sm text-white btn-lg main-color">フォロー中</a>
+                                <a href="#" id="follow_button" data-user-id="<?php echo esc_attr($one_follow->target_user_id); ?>" class="js-toggle-follow btn btn-primary rounded-pill btn-sm text-white btn-lg main-color follow-btn follow-on">フォロー中</a>
                             <?php } else { ?>
-                                <a href="#" id="follow_button" data-user-id="<?php echo esc_attr($one_follow->target_user_id); ?>" class="js-toggle-follow btn rounded-pill btn-outline-primary outline-btn btn-sm">フォローする</a>
+                                <a href="#" id="follow_button" data-user-id="<?php echo esc_attr($one_follow->target_user_id); ?>" class="js-toggle-follow btn py-0 rounded-pill btn-outline-primary btn-sm follow-btn follow-off">フォローする</a>
                             <?php } ?>
+                            </div>
                         </div>
-                    </div>
-                    <hr class="list-area-border my-0">
-                <?php }
-                /** endforeach **/ ?>
-            </ul>
+                        <hr class="list-area-border my-0">
+                    <?php }
+                    /** endforeach **/ ?>
+                </ul>
+            </div>
         </div>
     </div>
-</div>
-<?php
-get_footer();
+    <?php
+    get_footer();
