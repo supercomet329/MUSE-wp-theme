318bb11b3a838c40026f2756fbc5005ed435037a
supercomet329 <supercomet329@gmail.com>
2022-09-21 14:10:34 +0900

    # プロフィール編集ページの対応

--- muse_template/membership-template/edit_profile.php
+++ muse_template/membership-template/edit_profile.php
@@ -1,6 +1,6 @@
 <?php
 global $dp_options, $tcd_membership_vars;
 
-get_template_part( 'template-parts/header_non_pc_menu' );
+get_header();
 tcd_membership_edit_profile_form();
-get_template_part( 'template-parts/footer_non_pc_menu' );
+get_footer();
